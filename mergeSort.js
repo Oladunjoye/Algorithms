0a8d5e88614c980dfdc59a6fa05146ce0b35bc1b
Joye Shonubi <joye@omnibiz.com>
2020-03-26 17:44:52 +0100

    added sorting algorithms

---
+++ mergeSort.js
@@ -0,0 +1,39 @@
+
+function merge(arr1, arr2){
+
+    let newArr= []
+    let i =0
+    let j= 0
+
+    while(i < arr1.length  && j < arr2.length  ){
+        // console.log(arr1, arr2, arr1[i], arr2[j], newArr)
+        if(arr1[i] < arr2[j]){
+            newArr.push(arr1[i])
+            i++
+        }
+        else{
+            newArr.push(arr2[j])
+            j++
+        }
+    }
+
+
+    while(i < arr1.length){
+        newArr.push(arr1[i])
+        i++
+    }
+
+    while(j < arr2.length){
+        newArr.push(arr2[j])
+        j++
+    }
+
+    return newArr
+
+    // if(i < newArr.length -1){
+    //     [...newArr, ]
+    // }
+}
+
+
+console.log(merge([1,3,6], [2,4,9,12,22]))
--- selectionSort.js
+++ selectionSort.js
@@ -1,3 +1,12 @@
+//selections sort works similarly to bubble sort
+// it sorts by bringing the smallest values to the beginning of the array per iteration
+// it employs two loops
+//the outer loop esnures all the iterations are run in the worst case scenario
+// the inner loop makes the swaps and comparisons
+// the index of the minimum value (initially the  i) is stored
+// then comparisons are made with the subequent values
+//and at the end of the iteration a swap is made with whatever the minimum is
+
 function selectionSort(arr){
 
     const swap = (arr, i , j)=> {
@@ -8,7 +17,7 @@ function selectionSort(arr){
     let min= i
 
         for(let j = i + 1; j< arr.length; j++){
-            console.log(arr,arr[j],arr[min])
+            // console.log(arr,arr[j],arr[min])
 
             if(arr[j] < arr[min] ){
 
