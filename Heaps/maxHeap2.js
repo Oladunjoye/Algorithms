0d10b3af49348b29f7afd223671cd949527a1216
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-05-22 03:09:36 -0400

    Update maxHeap2.js

--- Heaps/maxHeap2.js
+++ Heaps/maxHeap2.js
@@ -4,11 +4,50 @@ class MaxHeap {
     this.elements = 0;
   }
 
-  insert(val) {}
+  insert(val) {
+    if (this.elements >= this.heap.length) {
+      this.heap.push(val);
+      this.elements++;
+      this.percolateUp(this.heap.length - 1);
+    } else {
+      this.heap[this.elements] = val;
+      this.elements++;
+      this.percolateUp(this.elements - 1);
+    }
+  }
+  getmax() {
+    if (this.elements != 0) {
+      return this.heap[0];
+    }
+    return null;
+  }
+  removeMax() {
+    if (this.elements > 1) {
+      let max = this.heap[0];
+      this.heap[0] = this.heap[this.elements - 1];
+      this.elements--;
+      this.maxHeapify(0);
+      return max;
+    } else if (this.elements == 1) {
+      let max = this.heap[0];
+      this.elements--;
+      return max;
+    } else {
+      return null;
+    }
+  }
 
-  removeMax() {}
+  percolateUp(index) {
+    let parent = Math.floor((n - 1) / 2);
+    if (index <= 0) {
+      return;
+    } else if (this.heap[parent] < this.heap[index]) {
+      let temp = this.heap[index];
+      this.heap[index] = this.heap[parent];
+      this.heap[parent] = temp;
+      this.percolateUp(parent);
+    }
+  }
 
-  percolateUp() {}
-
-  percolateDown() {}
+  maxHeapify() {}
 }
