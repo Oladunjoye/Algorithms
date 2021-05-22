cb1024b96fadf3a0eb08079d0388970064051e38
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-05-21 22:33:20 -0400

    Update maxHeap.js

--- Heaps/maxHeap.js
+++ Heaps/maxHeap.js
@@ -5,19 +5,33 @@ class MaxHeap {
 
   insert(val) {
     this.values.push(val);
-    let n = this.values.length
+    let n = this.values.length - 1;
 
-    let parent  =Math.floor((n-1)/2)
+    let parent = Math.floor((n - 1) / 2);
     // compare value with parent
 
-    while(this.values[n] < this.values[parent]){
+    while (this.values[n] > this.values[parent]) {
+      let temp = this.values[n];
+      this.values[n] = this.values[parent];
+      this.values[parent] = temp;
 
-      //swap 
+      //swap
 
       //update index
-
-      
+      n = parent;
+      parent = Math.floor((n - 1) / 2);
     }
-    
   }
 }
+
+let heap = new MaxHeap();
+
+heap.insert(7);
+heap.insert(12);
+heap.insert(3);
+heap.insert(2);
+heap.insert(22);
+heap.insert(1);
+heap.insert(5);
+
+console.log(heap.values);
