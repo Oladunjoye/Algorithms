9aa234eba15daf4797fdfc140e9d892358f8ca23
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-02 19:21:29 -0400

    Update howSum.js

--- DP/Memoization/howSum.js
+++ DP/Memoization/howSum.js
@@ -1,10 +1,25 @@
-function howSum(numbers, target){
+function howSum(numbers, target) {
+  // base cases
+  if (target == 0) return [];
+  if (target < 0) return null;
 
+  let result = [];
+  for (let num of numbers) {
+    let newTarget = target - num;
+
+    let value = howSum(numbers, newTarget);
+
+    if (value) {
+      value.push(num);
+      return value;
+    }
+  }
+
+  return null;
 }
 
-
-console.log(howSum([1,3], 7))
-// console.log(howSum([1,3], 7))
-// console.log(howSum([1,3], 7))
-// console.log(howSum([1,3], 7))
-// console.log(howSum([1,3], 7))
+console.log(howSum([5, 3, 4, 7], 7));
+console.log(howSum([3, 1], 7));
+console.log(howSum([2, 3], 7));
+console.log(howSum([3, 9], 7));
+console.log(howSum([7,14], 300))
