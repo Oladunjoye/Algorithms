e1dbc8cdc52534480f59eb82686f494dd62ee54b
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-02 12:08:27 -0400

    Update gridTraveller.js

--- DP/Memoization/gridTraveller.js
+++ DP/Memoization/gridTraveller.js
@@ -1,19 +1,20 @@
-function gridTraveller(m,n){
+function gridTraveller(m,n, memo = {}){
 
+  if(m+","+n in memo) return memo[m+","+n]
   if(m === 1 && n === 1) return 1
   if(m == 0 || n == 0) return 0
 
-  let sum = gridTraveller(m-1,n) + gridTraveller(m,n-1)
-
+  let sum = gridTraveller(m-1,n, memo) + gridTraveller(m,n-1, memo)
+  memo[m+","+n] = sum
   return sum
 
 }
 
 console.log(gridTraveller(0,0))
-// console.log(gridTraveller(0,0))
-// console.log(gridTraveller(0,0))
-// console.log(gridTraveller(0,0))
-// console.log(gridTraveller(0,0))
+console.log(gridTraveller(2,3))
+console.log(gridTraveller(1,1))
+console.log(gridTraveller(3,3))
+console.log(gridTraveller(18,18))
 // console.log(gridTraveller(0,0))
 
 
