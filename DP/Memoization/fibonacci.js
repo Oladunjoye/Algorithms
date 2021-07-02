6aabe5ef22cdd4514fb911c82b2968c4c49435e3
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-02 11:41:25 -0400

    Update fibonacci.js

--- DP/Memoization/fibonacci.js
+++ DP/Memoization/fibonacci.js
@@ -1,12 +1,12 @@
 function fibonacci(n, memo = {}){
  
-  if(memo[n])return memo[n]
+  if(n in memo)return memo[n]
   if(n <= 1)return 1
 
-let sum = fibonacci(n-1, memo) + (fibonacci(n-2),memo)
+let sum = fibonacci(n-1, memo) + fibonacci(n-2, memo)
 memo[n] = sum
 
-return sum
+return memo[n]
 
 }
 
