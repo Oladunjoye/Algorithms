62aa829605df73ea300dbf26e9ebd1af0f02279a
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 06:41:19 -0400

    Create bestSum.js

---
+++ DP/Memoization/bestSum.js
@@ -0,0 +1,13 @@
+function bestSum(numbers, target){
+
+  if(target == 0) return []
+  if(target <  0) return null
+
+  for(let num of numbers){
+    let newTarget = target - num
+  }
+
+}
+
+
+console.log(bestSum([3,4,1],7))
