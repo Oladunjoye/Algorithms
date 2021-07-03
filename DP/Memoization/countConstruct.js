f318181ca43022e82108b8582b4c5bda4c2eaa98
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 09:24:25 -0400

    Update countConstruct.js

--- DP/Memoization/countConstruct.js
+++ DP/Memoization/countConstruct.js
@@ -1,17 +1,21 @@
-function countConstruct(target, wordBank){
+function countConstruct(target, wordBank) {
+  if (target === '') return 1;
 
-  if(target === "") return 1
-
-  let count = 0
-  for(let word of wordBank){
-
-    if(target.startsWith(word)){
-      
+  let count = 0;
+  for (let word of wordBank) {
+    if (target.startsWith(word)) {
+      let newTarget = target.slice(word.length);
+      let newCount = countConstruct(newTarget, wordBank);
+      if (newCount) {
+        count += newCount;
+      }
     }
-
   }
+
+  return count;
 }
 
-
-
-console.log(countConstruct("",[]))
+// console.log(countConstruct("",[]))
+console.log(
+  countConstruct('skateboard', ['bo', 'd', 'ate', 't', 'ska', 'sk', 'boar'])
+);
