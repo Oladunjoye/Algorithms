d4b54d51d05af58f1e9c7d3ba6c946f2891bad15
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 17:45:41 -0400

    completed all construct solution

--- DP/Memoization/allConstruct.js
+++ DP/Memoization/allConstruct.js
@@ -1,22 +1,21 @@
-function allConstruct(target,wordBank){
-if(target == "") return [[]]
+function allConstruct(target, wordBank, memo = {}) {
+  if (target in memo) return memo[target];
+  if (target == '') return [[]];
 
-let result = []
+  let result = [];
 
-for(let word of wordBank){
-if(target.startsWith(word)){
-  let newTarget = target.slice(word.length)
-  let combination  = allConstruct(newTarget, wordBank)
-let newWay = combination.map((way)=>  [word,...way])
+  for (let word of wordBank) {
+    if (target.startsWith(word)) {
+      let newTarget = target.slice(word.length);
+      let combination = allConstruct(newTarget, wordBank);
+      let newWay = combination.map((way) => [word, ...way]);
 
-result.push(...newWay)
-
-
-}
-
-}
-
-return result 
+      result.push(...newWay);
+      memo[target] = result;
+    }
+  }
+  memo[target] = result;
+  return result;
 }
 
 console.log(allConstruct('purple', ['purp', 'p', 'ur', 'le', 'purpl']));
