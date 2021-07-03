5ac888e978aee268b247915b896e068159116994
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 09:11:28 -0400

    Create countConstruct.js

---
+++ DP/Memoization/countConstruct.js
@@ -0,0 +1,17 @@
+function countConstruct(target, wordBank){
+
+  if(target === "") return 1
+
+  let count = 0
+  for(let word of wordBank){
+
+    if(target.startsWith(word)){
+      
+    }
+
+  }
+}
+
+
+
+console.log(countConstruct("",[]))
