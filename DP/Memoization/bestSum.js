0f2378bc6d4122e28d1a136cc41307214de58e65
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 07:45:29 -0400

    Update bestSum.js

--- DP/Memoization/bestSum.js
+++ DP/Memoization/bestSum.js
@@ -3,11 +3,25 @@ function bestSum(numbers, target){
   if(target == 0) return []
   if(target <  0) return null
 
+  let result = null
   for(let num of numbers){
     let newTarget = target - num
-  }
 
+    let value = bestSum(numbers, newTarget)
+
+    if(value ){
+      let combination = [...value, num]
+
+      if(result === null || combination.length < result.length){
+        result = combination
+      }
+    }
+ 
+   
+  }
+ 
+  return result
 }
 
 
-console.log(bestSum([3,4,1],7))
+console.log(bestSum([6,3,4,1],7))
