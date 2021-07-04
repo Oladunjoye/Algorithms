81834285699fdf87f340815c14e46e8d3aaa6f1b
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 23:23:40 -0400

    Create countConstruct.js

---
+++ DP/Tabulation/countConstruct.js
@@ -0,0 +1,37 @@
+function countConstruct(target, wordBank) {
+  let table = Array(target.length + 1).fill(0);
+  table[0] = 1;
+
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i]) {
+      for (let word of wordBank) {
+        let current = target.slice(0, i);
+        let newString = current + word;
+        if (target.startsWith(newString)) {
+          table[i + word.length] += 1;
+        }
+      }
+    }
+  }
+
+  return table[target.length];
+}
+
+console.log(countConstruct('abc', ['ab', 'c', 'a', 'bc']));
+console.log(
+  countConstruct('eeeeeeeeeeeeeeeeeef', [
+    'eeeee',
+    'eeeeeeee',
+    'eeeeeeeeeeeeeeee',
+    'c',
+  ])
+);
+console.log(
+  countConstruct('skateboard', ['bo', 'd', 'ate', 't', 'ska', 'sk', 'boar'])
+);
+
+// console.log('abc'.slice(0, 0) == "");
+// console.log('abc'.slice(0, 1));
+// console.log("abc".slice(0))
+// console.log("abc".slice(0))
+// console.log(""+"ab")
