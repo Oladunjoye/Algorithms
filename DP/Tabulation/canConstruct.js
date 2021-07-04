2ce4b9d8bd4ed3558d365dd4a0c98f311684448e
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 23:06:06 -0400

    Update canConstruct.js

--- DP/Tabulation/canConstruct.js
+++ DP/Tabulation/canConstruct.js
@@ -1,7 +1,25 @@
-function canConstruct(target, wordBank){
+function canConstruct(target, wordBank) {
+  let table = Array(target.length + 1).fill(false);
+  table[0] = true;
 
+  for (let i = 0; i <= target.length; i++) {
+    if (table[i]) {
+      for (let word of wordBank) {
+        let current = target.slice(0, i);
+        let newString = current + word;
+        if (target.startsWith(newString)) {
+          table[i + word.length] = true;
+        }
+      }
+    }
+  }
+
+  return table[target.length];
 }
 
-
-
-console.log(canConstruct())
+console.log(canConstruct('abc', ['ab', 'c']));
+// console.log('abc'.slice(0, 0) == "");
+// console.log('abc'.slice(0, 1));
+// console.log("abc".slice(0))
+// console.log("abc".slice(0))
+// console.log(""+"ab")
