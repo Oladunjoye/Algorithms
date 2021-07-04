02a9188de6b147313d72255af8d8b3368a5bce25
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 22:40:54 -0400

    Create bestSum.js

---
+++ DP/Tabulation/bestSum.js
@@ -0,0 +1,19 @@
+function bestSum(target, numbers) {
+  let table = Array(target + 1).fill(null);
+  table[0] = [];
+
+  for (let i = 0; i <= target; i++) {
+    if (table[i]) {
+      for (let num of numbers) {
+        let newCombination = [...table[i], num];
+        if(  table[i + num]  == null || newCombination.length <= table[i + num].length ){
+          table[i + num] = newCombination;
+
+        }
+      }
+    }
+  }
+  return table[target];
+}
+
+console.log(bestSum(7, [1,3, 4,7]));
