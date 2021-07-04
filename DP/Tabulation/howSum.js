59c5ef896e7dde1ee846d37fbd74d578020631d6
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 21:19:26 -0400

    Update howSum.js

--- DP/Tabulation/howSum.js
+++ DP/Tabulation/howSum.js
@@ -1,23 +1,19 @@
-function howSum(target, numbers){
+function howSum(target, numbers) {
+  let table = Array(target + 1).fill(null);
+  table[0] = [];
 
-  let table = Array(target+1).fill(null)
-  table[0] = []
-
-  for(let i = 0; i <= target; i++){
-
-    if(table[i]){
-      for(let num of numbers){
-        table[i+num] = [...table[i], num]
+  for (let i = 0; i <= target; i++) {
+    if (table[i]) {
+      for (let num of numbers) {
+        table[i + num] = [...table[i], num];
       }
     }
-
   }
 
-
-  return table[target]
-
+  return table[target];
 }
 
-
-
-console.log(howSum(7,[2]))
+console.log(howSum(7, [2]));
+console.log(howSum(7, [5, 3, 4]));
+console.log(howSum(300, [7, 14]));
+console.log(howSum(8, [3, 5]));
