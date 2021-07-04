7a11661250c3e2eead6f1ff8f8d6fcbf60cea3a0
Oladunjoye <38365007+Oladunjoye@users.noreply.github.com>
2021-07-03 23:15:19 -0400

    Update canConstruct.js

--- DP/Tabulation/canConstruct.js
+++ DP/Tabulation/canConstruct.js
@@ -18,6 +18,18 @@ function canConstruct(target, wordBank) {
 }
 
 console.log(canConstruct('abc', ['ab', 'c']));
+console.log(
+  canConstruct('eeeeeeeeeeeeeeeeeef', [
+    'eeeee',
+    'eeeeeeee',
+    'eeeeeeeeeeeeeeee',
+    'c',
+  ])
+);
+console.log(
+  canConstruct('skateboard', ['bo', 'd', 'ate', 't', 'ska', 'sk', 'boar'])
+);
+
 // console.log('abc'.slice(0, 0) == "");
 // console.log('abc'.slice(0, 1));
 // console.log("abc".slice(0))
